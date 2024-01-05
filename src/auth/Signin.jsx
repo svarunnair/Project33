e37b3d58d1018b28ee23e109595168ce2fdfcdbb
Varun Nair <varunsnair1997@gmail.com>
2024-01-05 23:50:34 +0530

    authentication

--- src/auth/Signin.jsx
+++ src/auth/Signin.jsx
@@ -1,206 +1,132 @@
-import { Box, Input, Typography, styled } from '@mui/material';
+import { Box, Button, Input, styled } from '@mui/material';
 import React from 'react'
-import { useDispatch } from 'react-redux';
-import { postAuth } from '../Redux/auth/authAction';
-
 
 
 const OuterContainer = styled(Box)(({ theme }) => ({
-  border: "4px solid black",
-  display:"flex",
-  width:"100%",
-  height:"100%",
-  
+display:"flex",
+  [theme.breakpoints.down("xl")]: {},
+  [theme.breakpoints.down("lg")]: {},
+  [theme.breakpoints.down("md")]: {},
+  [theme.breakpoints.down("sm")]: {},
+  [theme.breakpoints.down("xs")]: {},
+}));
+
+const InnerContainer = styled(Box)(({ theme }) => ({
+
+// border:"2px solid green",
 
   [theme.breakpoints.down("xl")]: {},
   [theme.breakpoints.down("lg")]: {},
-  [theme.breakpoints.down("md")]: {
-    display:"flex",
-    flexDirection:"column",
-    width:"100%",
-    height:"50%",
-  },
+  [theme.breakpoints.down("md")]: {},
   [theme.breakpoints.down("sm")]: {},
   [theme.breakpoints.down("xs")]: {},
 }));
 
 const ImageBox = styled(Box)(({ theme }) => ({
+
+  // border:"2px solid blue",
   
-  width:"100%",
-  height:"80%",
+    [theme.breakpoints.down("xl")]: {},
+    [theme.breakpoints.down("lg")]: {},
+    [theme.breakpoints.down("md")]: {},
+    [theme.breakpoints.down("sm")]: {},
+    [theme.breakpoints.down("xs")]: {},
+  }));
 
-  [theme.breakpoints.down("xl")]: {},
-  [theme.breakpoints.down("lg")]: {},
-  [theme.breakpoints.down("md")]: {},
-  [theme.breakpoints.down("sm")]: {},
-  [theme.breakpoints.down("xs")]: {},
-}));
+  const TextBox = styled(Box)(({ theme }) => ({
 
-const FirstBox = styled(Box)(({ theme }) => ({
-  border: "2px solid red",
-  width:"50%",
-  height:"100%",
-  display:"flex",
-  flexDirection:"column",
-  justifyContent:'center',
-  gap:40,
+    // border:"2px solid green",
+    gap:10,
+    
+      [theme.breakpoints.down("xl")]: {},
+      [theme.breakpoints.down("lg")]: {},
+      [theme.breakpoints.down("md")]: {},
+      [theme.breakpoints.down("sm")]: {},
+      [theme.breakpoints.down("xs")]: {},
+    }));
+
+    const AuthBox = styled(Box)(({ theme }) => ({
+
+      // border:"2px solid green",
+      display:"flex",
+      paddingBottom:30,
+      paddingTop:30,
+      gap:20,
+        [theme.breakpoints.down("xl")]: {},
+        [theme.breakpoints.down("lg")]: {},
+        [theme.breakpoints.down("md")]: {},
+        [theme.breakpoints.down("sm")]: {},
+        [theme.breakpoints.down("xs")]: {},
+      }));
+
+
+      const DataBox = styled(Box)(({ theme }) => ({
+
+        border:"2px solid red",
+       padding:50,
+     
+        
+          [theme.breakpoints.down("xl")]: {},
+          [theme.breakpoints.down("lg")]: {},
+          [theme.breakpoints.down("md")]: {},
+          [theme.breakpoints.down("sm")]: {},
+          [theme.breakpoints.down("xs")]: {},
+        }));
+
+      
   
 
-  
+        const FirstBox = styled(Box)(({ theme }) => ({
 
-  [theme.breakpoints.down("xl")]: {},
-  [theme.breakpoints.down("lg")]: {},
-  [theme.breakpoints.down("md")]: {
-    width:"100%",
-  },
-  [theme.breakpoints.down("sm")]: {},
-  [theme.breakpoints.down("xs")]: {},
-}));
-
-const ImageDiv = styled(Box)(({ theme }) => ({
-  border: "2px solid green",
-display:"flex",
-background:"black",
-width:"50%",
-
-
-  [theme.breakpoints.down("xl")]: {},
-  [theme.breakpoints.down("lg")]: {},
-  [theme.breakpoints.down("md")]: {
-    width:"100%"
-  },
-  [theme.breakpoints.down("sm")]: {},
-  [theme.breakpoints.down("xs")]: {},
-}));
-
-const DetailBox = styled(Box)(({ theme }) => ({
-  border: "2px solid green",
-  textAlign:"left",
-  [theme.breakpoints.down("xl")]: {},
-  [theme.breakpoints.down("lg")]: {},
-  [theme.breakpoints.down("md")]: {},
-  [theme.breakpoints.down("sm")]: {},
-  [theme.breakpoints.down("xs")]: {},
-}));
-
-const ButtonBox = styled(Box)(({ theme }) => ({
-  border: "2px solid yellow",
-  display:"flex",
-  justifyContent:"center",
-  gap:10,
-  [theme.breakpoints.down("xl")]: {},
-  [theme.breakpoints.down("lg")]: {},
-  [theme.breakpoints.down("md")]: {
-    display:"flex",
-    flexDirection:"column",
-    justifyContent:"center",
-  },
-  [theme.breakpoints.down("sm")]: {},
-  [theme.breakpoints.down("xs")]: {},
-}));
+          border:"2px solid black",
+          display:"flex",
+          flexDirection:"column",
+          gap:20,
+          paddingTop:20,
+        
+          
+         
+          
+            [theme.breakpoints.down("xl")]: {},
+            [theme.breakpoints.down("lg")]: {},
+            [theme.breakpoints.down("md")]: {},
+            [theme.breakpoints.down("sm")]: {},
+            [theme.breakpoints.down("xs")]: {},
+          }));
 
 
-
-const AuthButton = styled(Box)(({ theme }) => ({
-  border: "1px solid black",
-  background:"black",
-  color:"white",
-  height:40,
-  
-  width:200,
-  [theme.breakpoints.down("xl")]: {},
-  [theme.breakpoints.down("lg")]: {},
-  [theme.breakpoints.down("md")]: {
-    width:"80%",
-  },
-  [theme.breakpoints.down("sm")]: {},
-  [theme.breakpoints.down("xs")]: {},
-}))
-const TextAuth = styled(Typography)(({ theme }) => ({
-  fontSize:12,
-  textAlign:"left",
-  [theme.breakpoints.down("xl")]: {},
-  [theme.breakpoints.down("lg")]: {},
-  [theme.breakpoints.down("md")]: {},
-  [theme.breakpoints.down("sm")]: {},
-  [theme.breakpoints.down("xs")]: {},
-}))
-
-const TextOne = styled(Typography)(({ theme }) => ({
-
-  fontSize:18,
-  textAlign:"center",
-  [theme.breakpoints.down("xl")]: {},
-  [theme.breakpoints.down("lg")]: {},
-  [theme.breakpoints.down("md")]: {
-    fontSize:12,
-  },
-  [theme.breakpoints.down("sm")]: {},
-  [theme.breakpoints.down("xs")]: {},
-}))
-
-
-const AuthButtonOne = styled(Box)(({ theme }) => ({
-  border: "1px solid black",
-  background:"black",
-  color:"white",
-  height:40,
-  width:400,
-  [theme.breakpoints.down("xl")]: {},
-  [theme.breakpoints.down("lg")]: {},
-  [theme.breakpoints.down("md")]: {
-    width:"80%",
-  },
-  [theme.breakpoints.down("sm")]: {},
-  [theme.breakpoints.down("xs")]: {},
-}))
-
-
-
+       
 
 function Signin() {
-const dispatch = useDispatch()
-
-const handleSignin = ()=>{
-let data = {
-  email:"varun2@gmail.com",
-  password:"1234"
-}
-
-  dispatch(postAuth(data))
-}
-
-
   return (
     <OuterContainer>
+      <InnerContainer sx={{background:"black",width:"50%"}}>
+        <ImageBox as={"img"} sx={{width:"100%"}} src="https://www.rado.com/media/sgecom_contentsystem/Warranty-Extension/warranty-login-page.jpg?im=Resize=(1800,1175),aspect=fill;Crop=(0,0,1800,1175),gravity=Center" />
+      
+      </InnerContainer>
 
-<ImageDiv>
-      <ImageBox as={"img"} src="https://www.rado.com/media/sgecom_contentsystem/Warranty-Extension/warranty-login-page.jpg?im=Resize=(1800,1175),aspect=fill;Crop=(0,0,1800,1175),gravity=Center"/>
 
-      </ImageDiv>
+      <InnerContainer>
+        <DataBox>
+    
+        <TextBox sx={{fontSize:30,textAlign:"left",fontWeight:400}}>Customer Login</TextBox>
+  
+        <AuthBox>
+          <Button sx={{color:"white",":hover":{background:"black"},background:"black",width:250,height:50,borderRadius:0}}>Signup with Google</Button>
+          <Button sx={{color:"white",":hover":{background:"black"},background:"black",width:250,height:50,borderRadius:0}}>Signup with Apple</Button>
+        </AuthBox>
 
-      <FirstBox>
-
-        <DetailBox>Customer Login</DetailBox>
-
-        <ButtonBox>
-<AuthButton>Signn in with Google</AuthButton>
-<AuthButton>Signn in with Apple</AuthButton>
-        </ButtonBox>
+        <FirstBox>
         <Input placeholder='Email'/>
+        </FirstBox>
+        <FirstBox>
         <Input placeholder='Password'/>
-
-        
-       
-        <AuthButtonOne onClick={handleSignin}>Sign In</AuthButtonOne>
-        <TextAuth>Forgoten password?</TextAuth>
-
-        <TextOne>_______________________OR_______________________</TextOne>
-
-        <AuthButtonOne>Create my account</AuthButtonOne>
+        </FirstBox>
+        <Button sx={{width:520,color:"white",background:"black",":hover":{background:"black"}}}>SignIn</Button>
        
 
-      </FirstBox>
+        </DataBox>
+      </InnerContainer>
 
     </OuterContainer>
   )
